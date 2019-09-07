865625a1ba28561d469f75bbd5d682e75331548b
homurax <homuraxenoblade@gmail.com>
2019-09-07 16:29:47 +0800

    update client task

--- src/main/java/com/homurax/chapter04/server/parallel/client/QueryTask.java
+++ src/main/java/com/homurax/chapter04/server/parallel/client/QueryTask.java
@@ -27,8 +27,7 @@ public class QueryTask implements Runnable {
 
         try (Socket echoSocket = new Socket("localhost", Constants.CONCURRENT_PORT);
              PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+             BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()))) {
 
             WDI wdi = data.get(randomGenerator.nextInt(data.size()));
 
@@ -46,7 +45,7 @@ public class QueryTask implements Runnable {
             String command = writer.toString();
             out.println(command);
             String output = in.readLine();
-            out.println(output);
+            System.out.println(output);
         } catch (Exception e) {
             e.printStackTrace();
         }
