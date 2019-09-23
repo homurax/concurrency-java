e1310e20bf8a755a4ec1967a15d418405efea3af
homurax <homuraxenoblade@gmail.com>
2019-09-23 23:05:44 +0800

    update chapter 07: Fork/Join

--- src/main/java/com/homurax/chapter07/filter/common/FilterData.java
+++ src/main/java/com/homurax/chapter07/filter/common/FilterData.java
@@ -1,23 +1,10 @@
 package com.homurax.chapter07.filter.common;
 
+import lombok.Data;
+
+@Data
 public class FilterData {
 	
 	private int idField;
 	private String value;
-	
-	public int getIdField() {
-		return idField;
-	}
-	public void setIdField(int idField) {
-		this.idField = idField;
-	}
-	public String getValue() {
-		return value;
-	}
-	public void setValue(String value) {
-		this.value = value;
-	}
-	
-	
-
 }
